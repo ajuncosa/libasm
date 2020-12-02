67c0d3739169669a32528c5d62d74f0fba7740a7
Ana Juncosa montes <ajuncosa@c1r5s1.42madrid.com>
2020-12-02 12:20:36 +0100

    add fixed ft_strcmp

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,1 +1,23 @@
-global	_ft_strcmp
+			global	_ft_strcmp
+
+			section	.text
+_ft_strcmp:	cmp		byte[rsi], 0
+			sete	dl
+			cmp		byte[rdi], 0
+			sete	dh
+			and		dl, dh
+			jnz		equal
+			movzx	cx, byte[rsi]
+			movzx	bx, byte[rdi]
+			cmp		bx, cx
+			jl		less
+			jg		more
+			add		rsi, 1
+			add		rdi, 1
+			jmp		_ft_strcmp
+less:		mov		rax, -1
+			ret
+more:		mov		rax, 1
+			ret
+equal:		mov		rax, 0
+			ret
--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 size_t	ft_strlen(char *);
 char	*ft_strcpy(char *dst, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+
 int main()
 {
 	char *src = "ktal";
@@ -19,18 +21,43 @@ int main()
 //	char *ret = ft_strcpy(str2, src);
 //	printf("str2 dir: %p\n", str2);
 // 	printf("strcpy ret: %p\n", ret);
-	
+/*	
 	printf("strcpy: %s\n", strcpy(dst1, "ho"));
 	printf("ft_strcpy: %s\n", ft_strcpy(dst2, "ho"));
 	printf("strcpy: %s\n", strcpy(dst1, src));
 	printf("ft_strcpy: %s\n", ft_strcpy(dst2, src));
 	printf("strcpy: %s\n", strcpy(dst1, "hola"));
-	printf("ft_strcpy: %s\n", ft_strcpy(dst2, "hola"));
+	printf("ft_strcpy: %s\n", ft_strcpy(dst2, "hola"));*/
 //	printf("strcpy: %s\n", strcpy(dst1, "holaa"));
 //	printf("ft_strcpy: %s\n", ft_strcpy(dst2, "holaa"));
 
 
 
 	/* STRCMP */
+/*	char *uno = "hola";
+	char *dos = "holaa";
+
+	printf("strcmp: %d\n", strcmp("hola", "holaa"));
+	printf("strcmp: %d\n", strcmp(uno, dos));
+	printf("ft_strcmp: %d\n", ft_strcmp(uno, dos));
+	printf("strcmp: %d\n", strcmp(uno, dos));
+	printf("ft_strcmp: %d\n", ft_strcmp(uno, dos));
+	printf("strcmp: %d\n", strcmp("hola", "hol"));
+	printf("ft_strcmp: %d\n", ft_strcmp("hola", "hol"));
+	printf("strcmp: %d\n", strcmp("hola", ""));
+	printf("ft_strcmp: %d\n", ft_strcmp("hola", ""));
+	printf("strcmp: %d\n", strcmp("hola", "hola"));
+	printf("ft_strcmp: %d\n", ft_strcmp("hola", "hola"));
+	printf("strcmp: %d\n", strcmp("", ""));
+	printf("ft_strcmp: %d\n", ft_strcmp("", ""));
+	printf("strcmp: %d\n", strcmp("hola", "adios"));
+	printf("ft_strcmp: %d\n", ft_strcmp("hola", "adios"));
+	//printf("\xff\xff\n\x31\n");
+	printf("strcmp: %d\n", strcmp("\xff\xff", "\xff"));
+	printf("ft_strcmp: %d\n", ft_strcmp("\xff\xff", "\xff"));
+	printf("strcmp: %d\n", strcmp("\xff", ""));
+	printf("ft_strcmp: %d\n", ft_strcmp("\xff", ""));*/
+
+
 	return (0);
 }
